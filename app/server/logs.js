d0c12eb879e5dac7244ef49544925588d54c12af
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-01-30 01:53:37 -0500

    modularized the logs endpoints as well

---
+++ app/server/logs.js
@@ -0,0 +1,63 @@
+/* global require */
+/* global module */
+// jshint esversion:6
+
+var Router = require('express').Router;
+
+module.exports = function(config) {
+    var logsRouter = new Router();
+
+    logsRouter.get('/', (req, res) => {
+        config.db.allDocs({ include_docs: true }).then(result => {
+            res.status(200).json(result);
+        }).catch(error => {
+            res.status(500).json(error);
+        });
+    });
+
+    logsRouter.post('/', (req, res) => {
+        req.body.date = req.body.date || new Date().toISOString();
+
+        config.db.post(req.body).then(result => {
+            res.status(201).json(result);
+        }).catch(error => {
+            res.status(500).json(error);
+        });
+    });
+
+    logsRouter.put('/:id', (req, res) => {
+        req.body._id = req.params.id;
+        config.db.get(req.params.id).then(result => {
+            req.body._rev = result._rev;
+            return config.db.put(req.body);
+        }).then(result => {
+            res.status(200).json(result);
+        }).catch(error =>{
+            if (error.status === 404) {
+                res.status(404).json('Document not found');
+            }
+            else {
+                res.status(500).json(error);
+            }
+        });
+    });
+
+    logsRouter.delete('/:id', (req, res) => {
+        req.body._id = req.params.id;
+        config.db.get(req.params.id).then(result =>{
+            req.body._rev = result._rev;
+            return config.db.remove(req.body);
+        }).then(result => {
+            res.status(200).json(result);
+        }).catch(error =>{
+            if (error.status === 404) {
+                res.status(404).json('Document not found');
+            }
+            else {
+                res.status(500).json(error);
+            }
+        });
+    });
+
+    return logsRouter;
+};
--- app/server/server.js
+++ app/server/server.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var chalk = require('chalk');
 var PouchDb = require('pouchdb');
 
+var logs = require('./logs');
 var reports = require('./reports');
 
 var db = new PouchDb('http://localhost:5984/logs');
@@ -27,59 +28,9 @@ apiRouter.get('/status', (req, res) => {
     res.status(200).json({ status: 'live' });
 });
 
-apiRouter.get('/logs', (req, res) => {
-    db.allDocs({ include_docs: true }).then(result => {
-        res.status(200).json(result);
-    }).catch(error => {
-        res.status(500).json(error);
-    });
-});
-
-apiRouter.post('/logs', (req, res) => {
-    req.body.date = req.body.date || new Date().toISOString();
-
-    db.post(req.body).then(result => {
-        res.status(201).json(result);
-    }).catch(error => {
-        res.status(500).json(error);
-    });
-});
-
-apiRouter.put('/logs/:id', (req, res) => {
-    req.body._id = req.params.id;
-    db.get(req.params.id).then(result => {
-        req.body._rev = result._rev;
-        return db.put(req.body);
-    }).then(result => {
-        res.status(200).json(result);
-    }).catch(error =>{
-        if (error.status === 404) {
-            res.status(404).json('Document not found');
-        }
-        else {
-            res.status(500).json(error);
-        }
-    });
-});
-
-apiRouter.delete('/logs/:id', (req, res) => {
-    req.body._id = req.params.id;
-    db.get(req.params.id).then(result =>{
-        req.body._rev = result._rev;
-        return db.remove(req.body);
-    }).then(result => {
-        res.status(200).json(result);
-    }).catch(error =>{
-        if (error.status === 404) {
-            res.status(404).json('Document not found');
-        }
-        else {
-            res.status(500).json(error);
-        }
-    });
-});
-
+apiRouter.use('/logs', logs({ db: db }));
 apiRouter.use('/reports', reports({ db: db }));
+
 app.use('/api/v1', apiRouter);
 
 app.listen(process.argv[2] || 8080);
