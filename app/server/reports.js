9f3878bd02732f85abf789fd6829dce1a15377e3
Braun Brennecke <bcbrennecke@gmail.com>
2016-02-03 21:29:28 -0500

    Added get/day call to get the current day's records

--- app/server/reports.js
+++ app/server/reports.js
@@ -8,7 +8,17 @@ module.exports = function(config) {
     var reportsRouter = new Router();
 
     reportsRouter.get('/day', function(req, res) {
-        // access the db using config.db
+        config.db.allDocs({ include_docs: true }).then(result => {
+            //console.log(result.rows[0])
+            var filtered = [];
+            for (var k = 0; k < result.rows.length; ++k) {
+                var item = result.rows[k];
+                if ((Date.parse(item.doc.date) > new Date().setTime(0,0,0,0)) && (Date.parse(item.doc.date) < new Date().setTime(23,59,59,0))) filtered.push(item);
+            }
+            res.status(200).json(filtered);
+        }).catch(error => {
+            res.status(500).json(error);
+        });
     });
 
     reportsRouter.get('/week', function(req, res) {
