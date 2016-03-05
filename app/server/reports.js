5ac2c9fb5fc78285392c4593acdbd4077e7b6ba5
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-03-05 00:13:55 -0500

    added an endpoint for weekly reports, refactored

--- app/server/reports.js
+++ app/server/reports.js
@@ -5,36 +5,62 @@
 var Router = require('express').Router;
 var _ = require('lodash');
 
-module.exports = function(config) {
-    var reportsRouter = new Router();
-
-    reportsRouter.get('/day', function(req, res) {
-        var now = new Date();
-        var start = new Date(
-            now.getFullYear(),
-            now.getMonth(),
-            now.getDate(),
+function getDayRange(date) {
+    return {
+        start: new Date(
+            date.getFullYear(),
+            date.getMonth(),
+            date.getDate(),
             0, 0, 0
-        );
-        var end = new Date(
-            now.getFullYear(),
-            now.getMonth(),
-            now.getDate(),
+        ),
+        end: new Date(
+            date.getFullYear(),
+            date.getMonth(),
+            date.getDate(),
             23, 59, 59
-        );
-        config.db.allDocs({ include_docs: true }).then(results => {
-            console.log(results);
+        )
+    };
+}
+
+function getWeekRange(date) {
+    var thisWeek = {};
+    var today = getDayRange(date);
+    var weekDay = today.start.getDay();
+    var MS_IN_A_DAY = 86400000;
+
+    var startOffset = weekDay === 0 ? weekDay + 6 : weekDay - 1;
+    var endOffset = weekDay === 0 ? 6 - (weekDay + 6) : 6 - (weekDay - 1);
+
+    thisWeek.start = new Date(today.start.getTime() - (MS_IN_A_DAY * startOffset));
+    thisWeek.end = new Date(today.end.getTime() + (MS_IN_A_DAY * endOffset));
+
+    return thisWeek;
+}
+
+function findDocsInRange(db, start, end) {
+    return new Promise((resolve, reject) => {
+        db.allDocs({ include_docs: true }).then(results => {
             var docs = [];
             _.forEach(results.rows, doc => {
                 var time = new Date(parseInt(doc.id.split('_')[1]));
-                console.log(time);
                 if (time.getTime() >= start.getTime() && time.getTime() <= end.getTime()) {
                     docs.push(doc);
                 }
             });
+            resolve(docs);
+        }).catch(reject);
+    });
+}
+
+module.exports = function(config) {
+    var reportsRouter = new Router();
+
+    reportsRouter.get('/day', function(req, res) {
+        var today = getDayRange(new Date());
+        findDocsInRange(config.db, today.start, today.end).then(docs => {
             res.status(200).json({
-                start: start.toString(),
-                end: end.toString(),
+                start: today.start.toString(),
+                end: today.end.toString(),
                 entries: docs
             });
         }).catch(error => {
@@ -43,7 +69,16 @@ module.exports = function(config) {
     });
 
     reportsRouter.get('/week', function(req, res) {
-
+        var thisWeek = getWeekRange(new Date());
+        findDocsInRange(config.db, thisWeek.start, thisWeek.end).then(docs => {
+            res.status(200).json({
+                start: thisWeek.start.toString(),
+                end: thisWeek.end.toString(),
+                entries: docs
+            });
+        }).catch(error => {
+            res.status(error.status).json(error);
+        });
     });
 
     return reportsRouter;
