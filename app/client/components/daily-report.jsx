ec26c2fa17b75b0ef2ecab85cdbaa71f747f395f
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-03-05 19:02:15 -0500

    added a view to display the daily report

---
+++ app/client/components/daily-report.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class DailyReport extends React.Component {
+
+    render() {
+        return (
+            <h2>future site of the daily report card/view/something</h2>
+        );
+    }
+}
