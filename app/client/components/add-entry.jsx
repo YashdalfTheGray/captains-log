ee3f9165dc986e3f71dcc6eafac38d83c76621ea
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-03-05 19:01:47 -0500

    added a view for adding entries to the log

---
+++ app/client/components/add-entry.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class AddEntry extends React.Component {
+
+    render() {
+        return (
+            <h2>future site of the add entry card/view/something</h2>
+        );
+    }
+}
