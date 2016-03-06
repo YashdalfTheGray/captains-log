026f58d5b755e4ae4b5358bd15e07fe38ec1e15a
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-03-05 19:02:39 -0500

    added a view to add the weekly report

---
+++ app/client/components/weekly-report.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class WeeklyReport extends React.Component {
+
+    render() {
+        return (
+            <h2>future site of the weekly report card/view/something</h2>
+        );
+    }
+}
